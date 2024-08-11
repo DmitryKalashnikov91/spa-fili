0071ecad1d7dd4f530bffe951c6118bb37b41007
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2024-08-11 11:34:49 +0300

    add ym count

---
+++ src/app/YandexMetrika.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function YandexMetrika() {
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+
+	useEffect(() => {
+		const url = `${pathname}?${searchParams}`
+		ym(98043134, 'hit', url)
+	}, [pathname, searchParams])
+
+	return null
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import Header from '@/components/header'
 import { El_Messiri } from 'next/font/google'
+import Script from 'next/script'
+import { Suspense } from 'react'
+import YandexMetrika from './YandexMetrika'
 
 import './globals.css'
 
@@ -13,6 +16,21 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={elMessiri.className}>
+				<Script id='metrika-counter' strategy='afterInteractive'>
+					{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(98043134, "init", {
+          defer: true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+    });`}
+				</Script>
 				<Header />
 
 				{children}
@@ -20,6 +38,9 @@ export default function RootLayout({
 					2024
 					<p>Спа Фили</p>
 				</footer>
+				<Suspense fallback={<></>}>
+					<YandexMetrika />
+				</Suspense>
 			</body>
 		</html>
 	)
