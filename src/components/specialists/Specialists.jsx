3c0610622e995273b6cfa902d94ea8bdd2aefef0
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2024-08-19 13:31:46 +0300

    add spec Elena

--- src/components/specialists/Specialists.jsx
+++ src/components/specialists/Specialists.jsx
@@ -1,49 +1,14 @@
-import { nanoid } from 'nanoid'
 import Image from 'next/image'
 import SubTitle from '../title/subTitle'
 import styles from './Specialists.module.scss'
-import Elena from './assets/elena.png'
-import konst from './assets/konst.png'
-import Mariya from './assets/mariya.png'
-import Olga from './assets/olga.png'
-
-const specData = [
-	{
-		id: nanoid(6),
-		name: 'Ольга',
-		urlImg: Olga,
-		content:
-			'7 лет стажа - все виды ручного массажа (Классический, Антицеллюлитный , Лимфодренажный, Аппаратный массаж (RF-лифтинг, Indiba, кавитация, вакуумный, вибрационный.), Коррекция фигуры, Скрабирование тела, Обертывание)',
-	},
-	{
-		id: nanoid(6),
-		name: 'Константин',
-		urlImg: konst,
-		content:
-			'20 лет стажа - все виды ручного массажа (Классический массаж,  Антицеллюлитный, Лимфодренажный, Спортивный, Баночный, Расслабляющий массаж, Массаж лица (классический)).',
-	},
-	{
-		id: nanoid(6),
-		name: 'Мария',
-		urlImg: Mariya,
-		content:
-			'Остеопат-массажист  - 15 лет стажа, мед.образование. Все виды массажа, плюс остеопатия.',
-	},
-	{
-		id: nanoid(6),
-		name: 'Елена',
-		urlImg: Elena,
-		content:
-			'Опыт 5,5 лет, все виды русного массажа: Классический, Релакс,  Лимфодренажный, Спортивный, Антицеллюлитный, Коррекция фигуры (Медовый, Солевой), Массаж лица (классика, скульптурный), Баночный.',
-	},
-]
+import { SpecJSON } from './SpecJson'
 
 const Specialists = () => {
 	return (
 		<>
 			<div className={styles.Specialists}>
 				<SubTitle color={'#018ABE'} content={'Специалисты'} />
-				{specData.map(spec => (
+				{SpecJSON.map(spec => (
 					<div className={styles.Specialists_wrapper} key={spec.id}>
 						<Image src={spec.urlImg} alt={spec.name} />
 						<div className={styles.Specialists_wrapper__content}>
