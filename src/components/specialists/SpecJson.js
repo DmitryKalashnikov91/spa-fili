2c1fdac743515f20fc0b57334283a50ad8449677
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2025-03-10 21:48:36 +0300

    change masters

--- src/components/specialists/SpecJson.js
+++ src/components/specialists/SpecJson.js
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid'
 import konst from './assets/konst.png'
 import Mariya from './assets/mary.png'
-import Oleg from './assets/oleg.png'
+import Oleg from './assets/Oleg2.png'
 import Olga from './assets/olga.png'
 
 export const SpecJSON = [
